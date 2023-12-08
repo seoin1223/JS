5e3e52382156e7b46ea1b60e5f7e27b8306e7495
서인 <seoin2389@icloud.com>
2023-12-09 05:48:46 +0900

    test; naming Convention

---
+++ 1_basics/4_naming_convertions.js
@@ -0,0 +1,24 @@
+/**
+ * Naming Conventions
+ * 1) 일반적으로 영어(라틴문자)를 사용하여 문자와 숫자를 모두 사용할 수 있다.
+ * 2) 특수기호는 _와 $만을 사용할 수 있다.
+ * 3) 숫자로 이름을 시작할 수 없다
+ * 4) 키워드는 변수명으로 사용할 수 없다
+ */
+
+let codefactory = '코트팩토리';
+var $iv = '아이브';
+const _yuJin = '안유진';
+
+// let 1name = 'no';
+// let const = 'const';
+
+/**
+ * Naming Convention 2
+ * 1) camelCase : 첫 글자는 소문자로 넣고, 띄어쓰기 할 때마다 첫 번째 글자를 대문자
+ * 2) snake_case : 띄어쓰기를 _ 로 표현
+ * 3) PascalCase : 첫 글자도 대문자로 넣고, 띄어쓰기 할 때마다 첫 번째 글자를 대문자로
+ */
+
+const anYuJin = '안유진'; // 카멜케이스
+console.log(anYuJin);
