72920a59cdeb6b021c6bb627826e952400a21f2a
서인 <seoin2389@icloud.com>
2023-12-09 05:47:03 +0900

    test vaildation test

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,38 @@
+/*
+* variable 선언하기
+* 1) var - 더이상 쓰지 않는다
+* 2) let
+* 3) const
+* */
+
+var name = '코트팩토리';
+console.log(name)
+
+let ive = '아이브';
+console.log(ive);
+
+/*
+* let과 var로 선언하면 추후 변경할 수 있다.
+*
+* */
+
+const newJeans = '코트팩토리';
+console.log(newJeans);
+
+/* const의 경우 값을 변경할 수 없다. */
+
+
+/**
+ * 선언과 할당
+ * 1) 선언 : 변수를 선언하는 것
+ * 2) 할당 : 변수에 값을 넣어주는 것 => const의 경우 선언할 떄 할당을 해야한다.
+ */
+
+var name1 ='코트팩토리';
+console.log(name1)
+
+let girlFrient;
+console.log(girlFrient);
+
+
+
