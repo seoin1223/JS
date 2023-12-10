e6c62e0d59e610492a5b71cc4fd5b31f8579f9e9
서인 <seoin2389@icloud.com>
2023-12-10 11:12:39 +0900

    DOC : update Operator

--- 1_basics/7_operator.js
+++ 1_basics/7_operator.js
@@ -57,3 +57,68 @@ console.log(typeof +sample);
 console.log(sample);
 console.log(typeof sample);
 console.log("----------------------")
+
+
+sample = false;
+console.log(+sample);
+console.log(typeof +sample)
+
+sample= '안유진';
+
+console.log(+sample)
+
+sample = '99';
+console.log(-sample);
+
+
+/* 할당 연산자 */
+number = 100;
+console.log(number)
+
+number+=10;
+console.log(number)
+
+console.log("비교 연산자 ----------------------")
+
+/* 비교 연산자
+* 1) 값의 비교
+* 2) 값과 타입의 비교
+* */
+console.log(5==5);
+console.log(5=='5');
+console.log(0=='');
+console.log(true==1);
+console.log(true=='1');
+console.log(false==0);
+
+console.log("-----------------------------")
+console.log(5===5);
+console.log(5==='5');
+console.log(0==='');
+
+/*
+* 대소 관계 비교 연산자
+* */
+
+console.log(100 >1 );
+console.log(100 <1 );
+console.log(100 <=1 );
+console.log(100 >=1 );
+
+/*
+* 단축 평가
+*  && 사용 했을 떄 좌측이 true이면 우측 값 반환
+*  && 사용 했을 때 좌측이 false면 좌측 값 반환
+*  || 사용했을 때 좌측이 true면 좌측 값 반환
+*  || 사용했을 때 좌측이 false면 우측값 반환
+* */
+console.log("단축 평가 -----------------")
+console.log(true && '아이브');
+console.log(false && '아이브');
+console.log(true || '아이브');
+console.log(false || '아이브');
+
+
+
+
+
